31f375f4490828ec40a3b6354acc98130685adf2
yashvardhan123 <73223247+yashvardhan123@users.noreply.github.com>
2020-10-21 16:25:50 +0530

    Delete dbGen.py

--- dbGen.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database.db')
-print('Opened database successfully')
-
-conn.execute('CREATE TABLE users (username TEXT, password TEXT)')
-
-print('Table created successfully')
-conn.close()
